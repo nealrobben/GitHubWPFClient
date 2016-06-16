f2ecbb91c4ccac8c843b4ce46860541781b198c9
Neal Robben <neal.robben@gmail.com>
2016-06-16 22:44:23 +0200

    Added unit test for status bar message

--- GitHubWPFClient/GitHubWPFClient.Tests/MainWindowViewModelTests.cs
+++ GitHubWPFClient/GitHubWPFClient.Tests/MainWindowViewModelTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using GitHubApiWrapper;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,19 @@ namespace GitHubWPFClient.Tests
         public void PassingNullToConstructorThrowsException()
         {
             Assert.Throws<ArgumentNullException>(() =>new MainWindowViewModel(null));
+        }
+
+        [Test]
+        public void GettingInvalidUserSetsStatusMessage()
+        {
+            var mock = new Mock<IGitHubWrapper>();
+            var vm = new MainWindowViewModel(mock.Object);
+            mock.Setup(foo => foo.GetUser("testuser")).Throws<ArgumentException>();
+
+            Assert.IsNull(vm.StatusMessage);
+            vm.UserName = "testuser";
+            vm.GetUserCommand.Execute(null);
+            Assert.IsNotNull(vm.StatusMessage);
         }
     }
 }
