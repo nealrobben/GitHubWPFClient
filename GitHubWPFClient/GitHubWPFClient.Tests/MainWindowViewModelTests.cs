dec8212e21b6dc4f6d54cf59072e23a76e8d303e
Neal Robben <neal.robben@gmail.com>
2016-06-17 15:53:31 +0200

    Added unit tests

--- GitHubWPFClient/GitHubWPFClient.Tests/MainWindowViewModelTests.cs
+++ GitHubWPFClient/GitHubWPFClient.Tests/MainWindowViewModelTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,10 +26,82 @@ namespace GitHubWPFClient.Tests
             var vm = new MainWindowViewModel(mock.Object);
             mock.Setup(foo => foo.GetUser("testuser")).Throws<ArgumentException>();
 
-            Assert.IsNull(vm.StatusMessage);
+            Assert.IsTrue(string.IsNullOrWhiteSpace(vm.StatusMessage));
             vm.UserName = "testuser";
             vm.GetUserCommand.Execute(null);
-            Assert.IsNotNull(vm.StatusMessage);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(vm.StatusMessage));
+        }
+
+        [Test]
+        public void GettingValidUserClearsStatusMessage()
+        {
+            var mock = new Mock<IGitHubWrapper>();
+            var vm = new MainWindowViewModel(mock.Object);
+            mock.Setup(foo => foo.GetUser("testuser")).Returns(new Mock<IUser>().Object);
+            mock.Setup(foo => foo.GetRepositoriesForUser("testuser")).Returns(new List<IRepository>());
+
+            vm.StatusMessage = "test";
+            vm.UserName = "testuser";
+            vm.GetUserCommand.Execute(null);
+            Assert.IsTrue(string.IsNullOrWhiteSpace(vm.StatusMessage));
+        }
+
+        [Test]
+        public void GettingValidUserSetsUserProperty()
+        {
+            var mock = new Mock<IGitHubWrapper>();
+            var vm = new MainWindowViewModel(mock.Object);
+            mock.Setup(foo => foo.GetUser("testuser")).Returns(new Mock<IUser>().Object);
+            mock.Setup(foo => foo.GetRepositoriesForUser("testuser")).Returns(new List<IRepository>());
+
+            Assert.IsNull(vm.User);
+            vm.UserName = "testuser";
+            vm.GetUserCommand.Execute(null);
+            Assert.IsNotNull(vm.User);
+        }
+
+        [Test]
+        public void GettingInvalidUserClearsUserProperty()
+        {
+            var mock = new Mock<IGitHubWrapper>();
+            var vm = new MainWindowViewModel(mock.Object);
+            mock.Setup(foo => foo.GetUser("testuser")).Throws<ArgumentException>();
+
+            var userMock = new Mock<IUser>();
+            vm.User = userMock.Object;
+            Assert.IsNotNull(vm.User);
+            vm.UserName = "testuser";
+            vm.GetUserCommand.Execute(null);
+            Assert.IsNull(vm.User);
+        }
+
+        [Test]
+        public void GettingValidUserSetsRepositoriesProperty()
+        {
+            var mock = new Mock<IGitHubWrapper>();
+            var vm = new MainWindowViewModel(mock.Object);
+            mock.Setup(foo => foo.GetUser("testuser")).Returns(new Mock<IUser>().Object);
+            mock.Setup(foo => foo.GetRepositoriesForUser("testuser")).Returns(new List<IRepository>());
+
+            Assert.IsNull(vm.Repositories);
+            vm.UserName = "testuser";
+            vm.GetUserCommand.Execute(null);
+            Assert.IsNotNull(vm.Repositories);
+        }
+
+        [Test]
+        public void GettingInvalidUserClearsRepositoriesProperty()
+        {
+            var mock = new Mock<IGitHubWrapper>();
+            var vm = new MainWindowViewModel(mock.Object);
+            mock.Setup(foo => foo.GetUser("testuser")).Throws<ArgumentException>();
+
+            var repositoriesMock = new ObservableCollection<IRepository>();
+            vm.Repositories = repositoriesMock;
+            Assert.IsNotNull(vm.Repositories);
+            vm.UserName = "testuser";
+            vm.GetUserCommand.Execute(null);
+            Assert.IsNull(vm.Repositories);
         }
     }
 }
